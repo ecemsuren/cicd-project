510205f68ce20e98e4915ae0c3f6ca6b28f3c4cc
ecem.suren <ecem.suren@adesso.com.tr>
2026-08-11 10:30:57 +0300

    test: invalidate keystore password to break CD pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
